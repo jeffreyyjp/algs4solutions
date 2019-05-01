8d39abf46f164f08b7a58b9c120d32dd74b3ba3c
Jeffrey_Yang <gracyme123@hotmail.com>
2019-05-01 10:26:05 +0800

    New solutions until to 1.5-11

---
+++ src/fundamentals/unionfind/WeightedQuickFindUF.java
@@ -0,0 +1,54 @@
+package fundamentals.unionfind;
+
+public class WeightedQuickFindUF {
+    private int[] id;  // access to component id (site indexed)
+    private int[] sz;    // size of component (site indexed)
+    private int count; // number of components
+
+    public WeightedQuickFindUF(int N) {
+        // Initialize component id array.
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+        sz = new int[N];
+        for (int i = 0; i < N; i++) sz[i] = 1;
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        return id[p];
+    }
+
+    public void union(int p, int q) {
+        // Put p and q into the same component.
+        int pID = find(p);
+        int qID = find(q);
+
+        // Nothing to do if p and q are already in the same component.
+        if (pID == qID) return;
+
+        // Rename p's component to q's name.
+        int smaller = pID;
+        int larger = qID;
+        if (sz[pID] > sz[qID]) {
+            smaller = qID;
+            larger = pID;
+        }
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == smaller) {
+                id[i] = larger;
+            }
+        }
+        sz[larger] += sz[smaller];
+        count--;
+    }
+}
