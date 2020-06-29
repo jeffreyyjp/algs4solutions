88e42ee854dbcebce8be6bab6f1110d33bbdabdb
Jeffrey Yang <gracyme123@hotmail.com>
2020-06-29 20:49:47 +0800

    implements floor() and ceiling() in BST

--- src/searching/binarysearchtrees/BST.java
+++ src/searching/binarysearchtrees/BST.java
@@ -22,7 +22,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 
     private int size(Node x) {
         if (x == null) return 0;
-        else           return x.N;
+        return x.N;
     }
 
     public Value get(Key key) {
@@ -34,9 +34,9 @@ public class BST<Key extends Comparable<Key>, Value> {
         // return null if key not present in subtree rooted at x.
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
-        if      (cmp < 0) return get(x.left, key);
-        else if (cmp > 0) return get(x.right, key);
-        else return x.val;
+        if (cmp < 0) return get(x.left, key);
+        if (cmp > 0) return get(x.right, key);
+        return x.val;
     }
 
     public void put (Key key, Value val) {
@@ -72,5 +72,37 @@ public class BST<Key extends Comparable<Key>, Value> {
     private Node max(Node x) {
         if (x.right == null) return x;
         return max(x.right);
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp < 0) return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        return x;
+    }
+
+    public Key ceiling(Key key) {
+        Node x = ceiling(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+
+    private Node ceiling(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp > 0) return ceiling(x.right, key);
+        Node t = ceiling(x.left, key);
+        if (t != null) return t;
+        return x;
     }
 }
