032def6e87ab5846ce47f3fba3c9fdb5ce6d687b
Jeffrey Yang <gracyme123@hotmail.com>
2020-07-24 21:32:08 +0800

    add dist to specific vertex in BFS

--- src/graphs/undirectedgraphs/BreadthFirstPaths.java
+++ src/graphs/undirectedgraphs/BreadthFirstPaths.java
@@ -5,19 +5,26 @@ import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
 
 public class BreadthFirstPaths {
+    private static final int INFINITY = Integer.MAX_VALUE;
     private boolean[] marked; // Is a shortest path to this vertex known?
     private int[] edgeTo; // last vertex on known path to this vertex.
+    private int[] distTo; // number of edges shortest s-v path
     private final int s; // source
 
     public BreadthFirstPaths(Graph G, int s) {
         marked = new boolean[G.V()];
         edgeTo = new int[G.V()];
+        distTo = new int[G.V()];
         this.s = s;
         bfs(G, s);
     }
 
     private void bfs(Graph G, int s) {
         Queue<Integer> queue = new Queue<Integer>();
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = INFINITY;
+        }
+        distTo[s] = 0;
         marked[s] = true; // Mark the source.
         queue.enqueue(s); // and put it on the queue.
         while (!queue.isEmpty()) {
@@ -25,6 +32,7 @@ public class BreadthFirstPaths {
             for (int w : G.adj(v)) {
                 if (!marked[w]) { // For every unmarked adjacent vertex.
                     edgeTo[w] = v; // save last edge on a shortest path.
+                    distTo[w] = distTo[v] + 1;
                     marked[w] = true; // mark it because path is known.
                     queue.enqueue(w); // and add it to the queue.
                 }
@@ -34,6 +42,10 @@ public class BreadthFirstPaths {
 
     public boolean hasPathTo(int v) {
         return marked[v];
+    }
+
+    public int distTo(int v) {
+        return distTo[v];
     }
 
     public Iterable<Integer> pathTo(int v) {
