d8575f2898c7c65c86a1e3639a3c6c0251720cb7
Jeffrey Yang <gracyme123@hotmail.com>
2020-07-25 15:06:22 +0800

    DepthFirstOrder imporvement

--- src/graphs/directedgraphs/DepthFirstOrder.java
+++ src/graphs/directedgraphs/DepthFirstOrder.java
@@ -6,14 +6,12 @@ import edu.princeton.cs.algs4.Stack;
 
 public class DepthFirstOrder {
     private boolean[] marked;
-    private Queue<Integer> pre; // vertices in preorder
-    private Queue<Integer> post; // vertices in postorder
-    private Stack<Integer> reversePost; // vertices in reverse postorder
+    private Queue<Integer> preorder; // vertices in preorder
+    private Queue<Integer> postorder; // vertices in postorder
 
     public DepthFirstOrder(Digraph G) {
-        pre = new Queue<Integer>();
-        post = new Queue<Integer>();
-        reversePost = new Stack<Integer>();
+        preorder = new Queue<Integer>();
+        postorder = new Queue<Integer>();
         marked = new boolean[G.V()];
 
         for (int v = 0; v < G.V(); v++) {
@@ -24,26 +22,28 @@ public class DepthFirstOrder {
     }
 
     private void dfs(Digraph G, int v) {
-        pre.enqueue(v);
+        preorder.enqueue(v);
         marked[v] = true;
         for (int w : G.adj(v)) {
             if (!marked[w]) {
                 dfs(G, w);
             }
         }
-        post.enqueue(v);
-        reversePost.push(v);
+        postorder.enqueue(v);
     }
 
     public Iterable<Integer> pre() {
-        return pre;
+        return preorder;
     }
 
     public Iterable<Integer> post() {
-        return post;
+        return postorder;
     }
 
     public Iterable<Integer> reversePost() {
-        return reversePost;
+        Stack<Integer> reverse = new Stack<Integer>();
+        for (int v : postorder)
+            reverse.push(v);
+        return reverse;
     }
 }
