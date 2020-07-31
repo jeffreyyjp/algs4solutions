f2825827bbb4ae4d1ffba794174cb98658bb0b89
Jeffrey Yang <gracyme123@hotmail.com>
2020-07-31 13:23:57 +0800

    Edge Weighted directed graph

---
+++ src/graphs/shortestpaths/EdgeWeightedDigraph.java
@@ -0,0 +1,42 @@
+package graphs.shortestpaths;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.DirectedEdge;
+
+public class EdgeWeightedDigraph {
+    private final int V; // number of vertices
+    private int E; // number of edges
+    private Bag<DirectedEdge>[] adj; // adjacency lists
+
+    public EdgeWeightedDigraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<DirectedEdge>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(DirectedEdge e) {
+        adj[e.from()].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> bag = new Bag<DirectedEdge>();
+        for (int v = 0; v < V; v++) {
+            for (DirectedEdge e : adj[v]) {
+                bag.add(e);
+            }
+        }
+        return bag;
+    }
+}
