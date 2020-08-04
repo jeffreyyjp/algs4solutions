78eec1a98ea1a694075ecdf19a69763ebd569ef6
Jeffrey Yang <gracyme123@hotmail.com>
2020-08-04 21:28:26 +0800

    BellmanFord algorithm for finding shortest path

---
+++ src/graphs/shortestpaths/BellmanFordSP.java
@@ -0,0 +1,70 @@
+package graphs.shortestpaths;
+
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.EdgeWeightedDirectedCycle;
+import edu.princeton.cs.algs4.Queue;
+
+public class BellmanFordSP {
+    private double[] distTo; // length of path to v
+    private DirectedEdge[] edgeTo; // last edge on shortest path
+    private boolean[] onQueue; // if v currently on the queue?
+    private Queue<Integer> queue; // queue of vertices to relax
+    private int cost; // number of calls to relax()
+    private Iterable<DirectedEdge> cycle;  // negative cycle (or null if no such cycle)
+
+    public BellmanFordSP(EdgeWeightedDigraph G, int s) {
+        distTo  = new double[G.V()];
+        edgeTo  = new DirectedEdge[G.V()];
+        onQueue = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        queue = new Queue<Integer>();
+        queue.enqueue(s);
+        onQueue[s] = true;
+        while (!queue.isEmpty() && !hasNegativeCycle()) {
+            int v = queue.dequeue();
+            onQueue[v] = false;
+            relax(G, v);
+        }
+    }
+
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (!onQueue[w]) {
+                    queue.enqueue(w);
+                    onQueue[w] = true;
+                }
+            }
+            if (cost++ % G.V() == 0) {
+                findNegativeCycle();
+                if (hasNegativeCycle()) return;  // found a negative cycle
+            }
+        }
+    }
+
+    public boolean hasNegativeCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<DirectedEdge> negativeCycle() {
+        return cycle;
+    }
+
+    private void findNegativeCycle() {
+        int V = edgeTo.length;
+        EdgeWeightedDigraph spt = new EdgeWeightedDigraph(V);
+        for (int v = 0; v < V; v++)
+            if (edgeTo[v] != null)
+                spt.addEdge(edgeTo[v]);
+
+        EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(spt);
+        cycle = finder.cycle();
+    }
+}
